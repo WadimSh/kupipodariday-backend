b1403555fa7fbe3cc7d839c468382c5289c228a4
WadimSh <wadim11@yandex.ru>
2024-06-26 11:53:20 +0300

    fix: добавлена гарда на роуты

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Req, UseGuards } from '@nestjs/common';
 import { Request as IRequest } from "express";
 
 import { UsersService } from './users.service';
 import { UserProfileResponseDto } from './dto/user-profile-response.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { FindUsersDto } from './dto/find-users.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 export interface RequestOneUser extends IRequest {
   user: UserProfileResponseDto;
@@ -14,34 +15,40 @@ export interface RequestOneUser extends IRequest {
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get('me')
   findOne(@Req() req: RequestOneUser) {
     return req.user;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch('me')
   async updateOne(@Req() req: RequestOneUser, @Body() updateUserDto: UpdateUserDto) {
     const user = await this.usersService.updateOne(req.user.id, updateUserDto);
     return user;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('me/wishes')
   async findOneUserWishes(@Req() req: RequestOneUser) {
     return await this.usersService.findOneUserWishes(req.user.id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':username')
   async getUser(@Param('username') username: string) {
     const user = await this.usersService.getUser(username);
     return user;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':username/wishes')
   async getUserWishes(@Param('username') username: string) {
     const wishes = await this.usersService.getUserWishes(username);
     return wishes;
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('find')
   async findMany(@Body() findUsersDto: FindUsersDto) {
     return await this.usersService.findMany(findUsersDto.query);
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -4,23 +4,5 @@ import { UpdateWishDto } from './dto/update-wish.dto';
 
 @Injectable()
 export class WishesService {
-  create(createWishDto: CreateWishDto) {
-    return 'This action adds a new wish';
-  }
-
-  findAll() {
-    return `This action returns all wishes`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} wish`;
-  }
-
-  update(id: number, updateWishDto: UpdateWishDto) {
-    return `This action updates a #${id} wish`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} wish`;
-  }
+  
 }
